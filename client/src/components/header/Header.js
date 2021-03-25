028bf26e0a1764c8b7f8a33f63ec78b9d7c1ccae
kaylamuraoka <kmurs98@gmail.com>
2021-03-25 13:26:56 -1000

    adding links to header

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="logo">
+        <h1><Link to="/">Fukunaga Engineers</Link></h1>
+      </div>
+
+      <ul>
+        <li><Link to="/"><i class="fas fa-shopping-cart"></i> Cart</Link></li>
+        <li><Link to="/login"><i class="fas fa-user"></i> Sign in</Link></li>
+      </ul>
+    </header>
   )
 }
 
