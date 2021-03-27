e6754829bf1aee998b71c314d27012affd495667
kaylamuraoka <kmurs98@gmail.com>
2021-03-26 21:46:48 -1000

    adding validation folder

---
+++ client/src/components/utils/validation/Validation.js
@@ -0,0 +1,29 @@
+export const isEmpty = value => {
+  if (!value) return true
+  return false
+}
+
+export const isEmail = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+export const isName = name => {
+  if (name.length < 2 || !name.match(/^[A-Za-z- ]+$/)) return false
+  return true
+}
+
+export const isPhone = phone => {
+  if (phone.length !== 10 || !phone.match(/^\d{10}$/)) return false
+  return true
+}
+
+export const isValidPassword = password => {
+  if (password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/)) return true
+  return false
+}
+
+export const isMatch = (password, cf_password) => {
+  if (password === cf_password) return true
+  return false
+}
