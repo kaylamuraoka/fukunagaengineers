fdc530f1722cb7601ca30b2a3f5ee3725d5fbcbc
kaylamuraoka <kmurs98@gmail.com>
2021-03-29 12:59:16 -1000

    updating profile component adding functionality

--- client/src/components/body/profile/Profile.js
+++ client/src/components/body/profile/Profile.js
@@ -7,6 +7,7 @@ import { showErrMsg, showSuccessMsg } from '../../utils/notification/Notificatio
 
 const initialState = {
   name: '',
+  phone: '',
   password: '',
   cf_password: '',
   err: '',
@@ -19,80 +20,168 @@ function Profile() {
 
   const { user, isAdmin } = auth
   const [data, setData] = useState(initialState)
+  const { name, phone, password, cf_password, err, success } = data
+
   const [avatar, setAvatar] = useState(false)
   const [loading, setLoading] = useState(false)
   const [callback, setCallback] = useState(false)
 
-  const { name, password, cf_password, err, success } = data
+  const handleChange = e => {
+    const { name, value } = e.target
+    setData({ ...data, [name]: value, err: '', success: '' })
+  }
+
+  const changeAvatar = async (e) => {
+    e.preventDefault()
+    try {
+      const file = e.target.files[0]
+
+      if (!file)
+        return setData({ ...data, err: "No files were uploaded.", success: '' })
+
+      if (file.size > 1024 * 1024)
+        return setData({ ...data, err: "The file that you are trying to upload exceeds the 1 MB size limit. Please select a file less than 1 MB.", success: '' })
+
+      if (file.type !== 'image/jpeg' && file.type !== 'image/png')
+        return setData({ ...data, err: "Invalid File format. You may only upload JPG, JPEG, PNG files.", success: '' })
+
+      let formData = new FormData()
+      formData.append('file', file)
+
+      setLoading(true)
+      const res = await axios.post('/api/upload_avatar', formData, {
+        headers: {
+          'content-type': 'multipart/form-data', Authorization: token
+        }
+      })
+
+      setLoading(false)
+      setAvatar(res.data.url)
+
+    } catch (err) {
+      setData({ ...data, err: err.response.data.msg, success: '' })
+    }
+
+  }
+
+  const updateInfo = () => {
+    try {
+      axios.patch('/user/update', {
+        name: name ? name : user.name,
+        phone: phone ? phone : user.phone,
+        avatar: avatar ? avatar : user.avatar,
+      }, {
+        headers: { Authorization: token }
+      })
+
+      setData({ ...data, err: '', success: 'Profile Updated Successfully!' })
+    } catch (err) {
+      setData({ ...data, err: err.response.data.msg, success: '' })
+    }
+  }
+
+  const updatePassword = () => {
+    if (!isValidPassword(password))
+      return setData({ ...data, err: "Password must be at least six characters long.", success: '' })
+
+    if (!isMatch(password, cf_password))
+      return setData({ ...data, err: "Passwords do not match.", success: '' })
+
+    try {
+      axios.post('/user/reset', { password }, {
+        headers: { Authorization: token }
+      })
+
+      setData({ ...data, err: '', success: 'Password Updated Successfully!' })
+    } catch (err) {
+      setData({ ...data, err: err.response.data.msg, success: '' })
+    }
+  }
+
+  const handleUpdate = () => {
+    if (name || phone || avatar) updateInfo()
+    if (password) updatePassword()
+  }
 
   return (
-    <div className="profile_page">
-      <div className="col-left">
-        <h2>{isAdmin ? 'Admin Profile' : 'User Profile'}</h2>
+    <>
+      {err && showErrMsg(err)}
+      {success && showSuccessMsg(success)}
+      {loading && <h3>Loading......</h3>}
+      <div className="profile_page">
+        <div className="col-left">
+          <h2>{isAdmin ? 'Admin Profile' : 'User Profile'}</h2>
 
-        <div className="avatar">
-          <img src={avatar ? avatar : user.avatar} alt="profile" />
-          <span>
-            <i className="fas fa-camera"></i>
-            <p>Change</p>
-            <input type="file" name="file" id="file_upload" />
-          </span>
+          <div className="avatar">
+            <img src={avatar ? avatar : user.avatar} alt="profile" />
+            <span>
+              <i className="fas fa-camera"></i>
+              <p>Change</p>
+              <input type="file" name="file" id="file_upload" onChange={changeAvatar} />
+            </span>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" defaultValue={user.name} placeholder="Your name" onChange={handleChange} />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" defaultValue={user.email} placeholder="Your email address" disabled />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="phone">Phone Number</label>
+            <input type="tel" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" name="phone" id="phone" defaultValue={user.phone} placeholder="Your phone number" onChange={handleChange} />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">New Password</label>
+            <input type="password" name="password" id="password" placeholder="Your password" value={password} onChange={handleChange} />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="cf_password">Confirm New Password</label>
+            <input type="password" name="cf_password" id="cf_password" placeholder="Confirm password" value={cf_password} onChange={handleChange} />
+          </div>
+
+          <div>
+            <em style={{ color: 'crimson' }}>
+              * If you update your password here, you will not be able to login quickly using google and facebook.
+          </em>
+          </div>
+
+          <button disabled={loading} onClick={handleUpdate}>Update</button>
         </div>
 
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" defaultValue={user.name} placeholder="Your name" />
-        </div>
+        <div className="col-right">
+          <h2>{isAdmin ? "Users" : "My Orders"}</h2>
 
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" defaultValue={user.email} placeholder="Your email address" disabled />
-        </div>
+          <div style={{ overflowX: "auto" }}>
+            <table className="users">
+              <thead>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Admin</th>
+                <th>Action</th>
+              </thead>
+              <tbody>
+                <td>ID</td>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Phone</td>
+                <td>Admin</td>
+                <td>Action</td>
 
-        <div className="form-group">
-          <label htmlFor="phone">Phone Number</label>
-          <input type="tel" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" name="phone" id="phone" defaultValue={user.phone} placeholder="Your phone number" disabled />
+              </tbody>
+            </table>
+          </div>
         </div>
-
-        <div className="form-group">
-          <label htmlFor="password">New Password</label>
-          <input type="password" name="password" id="password" placeholder="Your password" value={password} />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="cf_password">Confirm New Password</label>
-          <input type="password" name="cf_password" id="cf_password" placeholder="Confirm password" value={cf_password} />
-        </div>
-
-        <button disabled={loading}>Update</button>
       </div>
-
-      <div className="col-right">
-        <h2>{isAdmin ? "Users" : "My Orders"}</h2>
-
-        <div style={{ overflowX: "auto" }}>
-          <table className="users">
-            <thead>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-              <th>Admin</th>
-              <th>Action</th>
-            </thead>
-            <tbody>
-              <td>ID</td>
-              <td>Name</td>
-              <td>Email</td>
-              <td>Phone</td>
-              <td>Admin</td>
-              <td>Action</td>
-
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
 
