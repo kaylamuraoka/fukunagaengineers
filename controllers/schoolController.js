c9c1358a913fab916f84ab2e6fde916886ecf945
kaylamuraoka <kmurs98@gmail.com>
2021-03-31 11:38:45 -1000

    updated deleteSchool and updateSchool functions

--- controllers/schoolController.js
+++ controllers/schoolController.js
@@ -36,14 +36,25 @@ const schoolController = {
   },
   deleteSchool: async (req, res) => {
     try {
-
+      await Schools.findByIdAndDelete(req.params.id)
+      res.json({ msg: "Deleted a school." })
     } catch (err) {
       return res.status(500).json({ msg: err.message })
     }
   },
   updateSchool: async (req, res) => {
     try {
+      const { doe_code, name, address, city, zip_code, phone, fax, grade_from, grade_to, type, website, complex, island, charter, esis_name, principal, images, district, year_built } = req.body
 
+      if (!images)
+        return res.status(400).json({ msg: "No image upload" })
+
+      await Schools.findOneAndUpdate({ _id: req.params.id },
+        {
+          doe_code, name: name.toLowerCase(), address, city, zip_code, phone, fax, grade_from, grade_to, type, website, complex, island, charter, esis_name, principal, images, district, year_built
+        })
+
+      res.json({ msg: "Updated a school" })
     } catch (err) {
       return res.status(500).json({ msg: err.message })
     }
