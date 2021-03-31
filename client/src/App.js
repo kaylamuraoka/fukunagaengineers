328732d4070842ddb3411b311b5455904b199545
kaylamuraoka <kmurs98@gmail.com>
2021-03-30 15:48:17 -1000

    removing App.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react"
-import "./App.css"
 import { BrowserRouter as Router } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { dispatchLogin, fetchUser, dispatchGetUser } from "./redux/actions/authAction"
